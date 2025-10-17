8b7b7032691c8708e02b46baddaabe1208e50c56
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-10-17 12:10:08 -0300

    feat: adição de comandos para ativar rls nas tabelas

--- backend/row_level_security.sql
+++ backend/row_level_security.sql
@@ -1,3 +1,11 @@
+
+-- Comandos para Ativar a Row Level Security para todas as tabelas
+ALTER TABLE clientes ENABLE ROW LEVEL SECURITY;
+ALTER TABLE produtos ENABLE ROW LEVEL SECURITY;
+ALTER TABLE pedidos ENABLE ROW LEVEL SECURITY;
+ALTER TABLE itens_pedido ENABLE ROW LEVEL SECURITY;
+
+
 -- Política de leitura dos produtos
 -- Produtos podem ser consultados por todos os usuários
 create policy "Leitura pública dos produtos"
