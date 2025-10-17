7df58b4da04753a30fac0e44f7271a5cab93159f
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-10-17 01:20:57 -0300

    fix e docs: comentários e correção na política de rls

--- backend/row_level_security.sql
+++ backend/row_level_security.sql
@@ -1,7 +1,9 @@
+-- Política de leitura dos produtos
+-- Produtos podem ser consultados por todos os usuários
 create policy "Leitura pública dos produtos"
 on "public"."produtos"
 as PERMISSIVE
-for ALL
+for SELECT
 to public
 using (
     true
