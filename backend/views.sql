468c2ded1708dffdb0fa99793e1a29ceda7054c0
NattanSouza404 <nattanssouzapvz@gmail.com>
2025-10-17 01:23:35 -0300

    fix: conserto na parte de segurança das views

--- backend/views.sql
+++ backend/views.sql
@@ -1,5 +1,5 @@
 -- View de itens vendidos (a partir dela é possível montar um pedido do cliente)
-CREATE VIEW vw_itens_vendidos AS
+CREATE VIEW vw_itens_vendidos WITH (security_invoker = on) AS
 	SELECT
 		ite_ped_id AS id_pedido,
 		prd_nome AS produto,
@@ -15,8 +15,8 @@ CREATE VIEW vw_itens_vendidos AS
 	ORDER BY
 		ite_ped_id DESC;
 
--- Uma view geral sobre os pedidos
-CREATE VIEW vw_pedidos AS 
+-- Uma view geral sobre os pedidos (se usa ela na edge function de enviar e-mail)
+CREATE VIEW vw_pedidos WITH (security_invoker = on) AS  
   SELECT
 		ped_id AS id_pedido,
 		ped_created_at AS data_pedido,
@@ -29,4 +29,4 @@ CREATE VIEW vw_pedidos AS
 		JOIN itens_pedido ON ite_ped_id = ped_id
   GROUP BY
     1, 2, 3, 4
-  ORDER BY ped_created_at DESC;
+  ORDER BY ped_id DESC;
